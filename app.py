6e4faba107ccd26e689fc27dc437332dbd317ce7
samzhuwj <samzhuwj@gmail.com>
2018-01-06 23:51:01 +0800

    importe resources Comment to app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Blueprint
 from flask_restful import Api
 from resources.Hello import Hello
 from resources.Category import CategoryResource
+from resources.Comment import CommentResource
 
 api_bp = Blueprint('api', __name__)
 api = Api(api_bp)
@@ -9,3 +10,4 @@ api = Api(api_bp)
 # route
 api.add_resource(Hello,'/Hello')
 api.add_resource(CategoryResource, '/Category')
+api.add_resource(CommentResource, '/Comment')
