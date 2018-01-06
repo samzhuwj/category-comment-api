1061842c864f405ea12f3b31b1d022af270f06f4
samzhuwj <samzhuwj@gmail.com>
2018-01-06 23:38:42 +0800

    import resources Category to app.py

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Blueprint
 from flask_restful import Api
 from resources.Hello import Hello
+from resources.Category import CategoryResource
 
 api_bp = Blueprint('api', __name__)
 api = Api(api_bp)
 
 # route
 api.add_resource(Hello,'/Hello')
+api.add_resource(CategoryResource, '/Category')
